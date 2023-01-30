991b0301495fbf3aa9e61b198909f38b3c284310
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2023-01-30 21:27:26 +0000

    2023年01月31日 - 01

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,20 @@
 'use strict';
 
+const request = require('request');
+
 module.exports = function(app) {
   app.route('/api/stock-prices')
     // URL/api/stock-prices?stock=GOOG
+    //{ stock: 'GOOG', like: 'false' }
+    //{ stock: 'GOOG', like: 'true' }
+    //{ stock: [ 'GOOG', 'MSFT' ], like: 'false' }
+    //{ stock: [ 'GOOG', 'MSFT' ], like: 'true' }
     .get(function(req, res) {
-      console.log(req.params);
+      let symbol = req.query.stock;
+      let target = `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${symbol}/quote`;
+      request(target, (err, res, body) => {
+        console.log(JSON.parse(res.body));
+      });
     }
   );
 };
