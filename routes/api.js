6d1cebdfa200eb7409a172e1087af79ec4a74166
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2023-01-31 03:32:32 +0000

    2023年01月31日 - 02

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,6 @@ const request = require('request');
 
 module.exports = function(app) {
   app.route('/api/stock-prices')
-    // URL/api/stock-prices?stock=GOOG
     //{ stock: 'GOOG', like: 'false' }
     //{ stock: 'GOOG', like: 'true' }
     //{ stock: [ 'GOOG', 'MSFT' ], like: 'false' }
@@ -13,7 +12,23 @@ module.exports = function(app) {
       let symbol = req.query.stock;
       let target = `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${symbol}/quote`;
       request(target, (err, res, body) => {
-        console.log(JSON.parse(res.body));
+        // Step1
+        //console.log(res.body['latestPrice']);
+        //console.log(res.body.hasOwnProperty('symbol'));
+        //console.log(res.body.hasOwnProperty('latestPrice'));
+        //console.dir(res.body);
+        console.log(res.body['avgTotalVolume']);
+        //let price = res.body.latestPrice;
+        // Step2
+        //let object = {};
+        //object.stock = symbol;
+        //object.price = price;
+        // Step3
+        //let result = {};
+        //result.stockdata = object;
+        //console.log(result);
+        // Step4
+        //return JSON.parse(result);
       });
     }
   );
