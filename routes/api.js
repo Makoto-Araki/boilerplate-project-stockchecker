bd99ea33e4318be79424e17202421ad210c05e06
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2023-02-05 02:19:12 +0000

    2023年02月05日 - 01

--- routes/api.js
+++ routes/api.js
@@ -1,33 +1,51 @@
 'use strict';
 
+// Import Module
 const request = require('request');
+const mongodb = require('mongodb');
 
+// Constant for Proxy API
+const proxy = 'https://stock-price-checker-proxy.freecodecamp.rocks';
+const version = 'v1';
+
+// Constant for Mongo Collection
+const client = mongodb.MongoClient;
+
+// Get Stock Price from Proxy API
+const getStockPriceFromAPI = function(name) {
+  return new Promise(function(resolve) {
+    let option = {
+      url: `${proxy}/${version}/stock/${name}/quote`,
+      method: 'GET',
+      json: true
+    }
+    request(option, (error, response, body) => {
+      if (!error) {
+        resolve(body.latestPrice);
+      }
+    });
+  });
+}
+
+// Get Stock likes from Mongo Collection
+const getStockLikesFromCollection = function() {}
+
+//{ stock: 'GOOG', like: 'false' }
+//{ stock: 'GOOG', like: 'true' }
+//{ stock: [ 'GOOG', 'MSFT' ], like: 'false' }
+//{ stock: [ 'GOOG', 'MSFT' ], like: 'true' }
+
+// Main Process
 module.exports = function(app) {
   app.route('/api/stock-prices')
-    //{ stock: 'GOOG', like: 'false' }
-    //{ stock: 'GOOG', like: 'true' }
-    //{ stock: [ 'GOOG', 'MSFT' ], like: 'false' }
-    //{ stock: [ 'GOOG', 'MSFT' ], like: 'true' }
     .get(function(req, res) {
-      let proxy = 'https://stock-price-checker-proxy.freecodecamp.rocks';
-      let vers = 'v1';
-      let name = req.query.stock;
-      let like = req.query.like;
-      let option = {
-        url: `${proxy}/${vers}/stock/${name}/quote`,
-        method: 'GET',
-        json: true
-      }
-      let temp = {};
-      let result = {};
-      request(option, (error, response, body) => {
-        if (!error) {
-          temp.stock = req.query.stock;
-          temp.price = body.latestPrice;
-          result.stockData = temp;
-          res.send(result);
-        }
-      });
+      let result1;
+      let result2;
+      result1 = getStockPriceFromAPI(req.query.stock);
+      result1
+        .then(function(data) {
+          console.log(data);
+        });
     }
   );
 };
