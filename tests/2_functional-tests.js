7a30d1014d6f6cc2c782fcb2b22a5a413460c7e9
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2023-02-08 11:10:21 +0000

    2023年02月08日 - 05

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -2,9 +2,24 @@ const chaiHttp = require('chai-http');
 const chai = require('chai');
 const assert = chai.assert;
 const server = require('../server');
+const api = require('../routes/api');
 
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-
+  test('Viewing one stock', function(done) {
+    chai
+      .request(server)
+      .get('/api/stock-prices')
+      .query({ stock: 'AAPL' })
+      .end(function(err, res) {
+        
+        //let price = api.getStockPrice('AAPL');
+        assert.equal(res.status, 200);
+        assert.equal(res.type, 'application/json');
+        //assert.equal(res.stockData.stock, 'AAPL');
+        //assert.equal(res.stockData.price, price);
+        done();
+      });
+  });
 });
