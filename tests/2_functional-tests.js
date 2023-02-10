f083dea2ac8b94625797fd85a692b0165e2d6dda
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2023-02-10 10:27:34 +0000

    2023年02月10日 - 01

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -7,31 +7,77 @@ const mod2 = require('../modules/mod2');
 
 chai.use(chaiHttp);
 
-// Main Process
-const mainProcess = async function(name) {
+// Function for Get Stock Price
+const getStockPrice = async function(name) {
   let price = await mod1.getStockPrice(name);
-  let likes = await mod2.getStockLikes(name);
-  return { price: price, likes: likes };
+  return price;
 }
 
+// Variables for Stock Price
+let stock1 = 0;
+let stock2 = 0;
+
+// Get Stock Price of AAPL
+getStockPrice('AAPL').then(function(result) {
+  stock1 = result;
+});
+
+// Get Stock Price of NFLX
+getStockPrice('NFLX').then(function(result) {
+  stock2 = result;
+});
+
 suite('Functional Tests', function() {
-  /* ------------------------------------------------------------ *
+  /* ------------------------------------------------------------ */
   test('Viewing one stock', function(done) {
     chai
       .request(server)
       .get('/api/stock-prices')
       .query({ stock: 'AAPL' })
       .end(function(err, res) {
-        mainProcess('AAPL')
-          .then(function(result) {
-            assert.equal(res.body.stockData.stock, 'AAPL');
-            assert.equal(res.body.stockData.price, result.price);
-            assert.equal(res.body.stockData.likes, result.likes);
-          });
+        assert.equal(res.body.stockData.stock, 'AAPL');
+        assert.equal(res.body.stockData.price, stock1);
+        assert.equal(res.body.stockData.likes, 0);
         done();
       });
   });
-  /* ------------------------------------------------------------ *
-  //
+  /* ------------------------------------------------------------ */
+  test('Viewing one stock and liking it', function(done) {
+    chai
+      .request(server)
+      .get('/api/stock-prices')
+      .query({ stock: 'AAPL', like: 'true' })
+      .end(function(err, res) {
+        assert.equal(res.body.stockData.stock, 'AAPL');
+        assert.equal(res.body.stockData.price, stock1);
+        assert.equal(res.body.stockData.likes, 0);
+        done();
+      });
+  });
+  /* ------------------------------------------------------------ */
+  test('Viewing the same stock and liking it again', function(done) {
+    chai
+      .request(server)
+      .get('/api/stock-prices')
+      .query({ stock: 'AAPL', like: 'true' })
+      .end(function(err, res) {
+        assert.equal(res.body.stockData.stock, 'AAPL');
+        assert.equal(res.body.stockData.price, stock1);
+        assert.equal(res.body.stockData.likes, 1);
+        done();
+      });
+  });
+  /* ------------------------------------------------------------ */
+  test('Viewing two stocks', function(done) {
+    chai
+      .request(server)
+      .get('/api/stock-prices')
+      .query({ stock: ['AAPL', 'NFLX'] })
+      .end(function(err, res) {
+        assert.isArray(res.body.stockData);
+        done();
+      });
+  });
+  /* ------------------------------------------------------------ */
   /* ------------------------------------------------------------ */
 });
